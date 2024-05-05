6aed72e338fb488387ba4d428d45ca4fa8b271f3
Jyoti <j2530046@gmail.com>
2024-05-05 11:40:40 +0530

    my commit to tourism website

---
+++ src/components/Hotels.jsx
@@ -0,0 +1,27 @@
+const dubaiHotelData=[
+    {id : "1",
+    imgsrc : require("../img/dubaiHotelData1.jfif"),
+    link : "https://www.agoda.com/en-in/marriott-hotel-al-jaddaf-dubai/hotel/dubai-ae.html?finalPriceView=1&isShowMobileAppPrice=false&cid=1914936&numberOfBedrooms=&familyMode=false&adults=1&children=0&rooms=1&maxRooms=0&checkIn=2024-05-12&isCalendarCallout=false&childAges=&numberOfGuest=0&missingChildAges=false&travellerType=-1&showReviewSubmissionEntry=false&currencyCode=INR&isFreeOccSearch=false&tag=b14c17e5-64ec-4e75-8b49-2b25c8c29d4d&isCityHaveAsq=false&filterBenefits=78322&los=1&searchrequestid=1764d798-8c85-4114-a587-5c2843f3f862&ds=WoO4WV8bs3Y6m0FF",
+    place : "Marriott Hotel Al Jaddaf, Dubai",
+    price : "₹8,181"},
+
+    {id : "2",
+    imgsrc : require("../img/dubaiHotelData2.jfif"),
+    link : "https://www.agoda.com/en-in/coral-dubai-deira-hotel/hotel/dubai-ae.html?finalPriceView=1&isShowMobileAppPrice=false&cid=1914936&numberOfBedrooms=&familyMode=false&adults=1&children=0&rooms=1&maxRooms=0&checkIn=2024-05-12&isCalendarCallout=false&childAges=&numberOfGuest=0&missingChildAges=false&travellerType=-1&showReviewSubmissionEntry=false&currencyCode=INR&isFreeOccSearch=false&tag=b14c17e5-64ec-4e75-8b49-2b25c8c29d4d&isCityHaveAsq=false&tspTypes=9%2C16%2C8&filterBenefits=78322&los=1&searchrequestid=1764d798-8c85-4114-a587-5c2843f3f862&ds=WoO4WV8bs3Y6m0FF",
+    place : "Coral Dubai Deira Hotel",
+    price : "₹4,295"},
+    {id : "3",
+    imgsrc : require("../img/hotel dubai data 3.jfif"),
+    link : "https://www.agoda.com/en-in/taj-dubai_7/hotel/all/dubai-ae.html?finalPriceView=1&isShowMobileAppPrice=false&cid=1914936&numberOfBedrooms=&familyMode=false&adults=1&children=0&rooms=1&maxRooms=0&checkIn=2024-05-12&isCalendarCallout=false&childAges=&numberOfGuest=0&missingChildAges=false&travellerType=-1&showReviewSubmissionEntry=false&currencyCode=INR&isFreeOccSearch=false&tag=b14c17e5-64ec-4e75-8b49-2b25c8c29d4d&isCityHaveAsq=false&filterBenefits=78322&los=1&searchrequestid=3519f44a-7d82-4586-8401-5b60cd24da2e&ds=WoO4WV8bs3Y6m0FF",
+    place : "Taj Dubai",
+    price : "₹ 17,839"},
+
+    {id : "4",
+    imgsrc : require("../img/hotel data 4.jfif"),
+    link : "https://www.agoda.com/en-in/habtoor-grand-resort-autograph-collection/hotel/dubai-ae.html?finalPriceView=1&isShowMobileAppPrice=false&cid=1914936&numberOfBedrooms=&familyMode=false&adults=1&children=0&rooms=1&maxRooms=0&checkIn=2024-05-12&isCalendarCallout=false&childAges=&numberOfGuest=0&missingChildAges=false&travellerType=-1&showReviewSubmissionEntry=false&currencyCode=INR&isFreeOccSearch=false&tag=b14c17e5-64ec-4e75-8b49-2b25c8c29d4d&isCityHaveAsq=false&tspTypes=16&filterBenefits=78322&los=1&searchrequestid=3519f44a-7d82-4586-8401-5b60cd24da2e&ds=WoO4WV8bs3Y6m0FF",
+    place : "Habtoor Grand Resort, Autograph Collection",
+    price : "₹13,408"}
+
+];
+
+export default dubaiHotelData;
